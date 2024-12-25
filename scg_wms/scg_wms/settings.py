ee16f18dca6924547a9326f44e7dc5c007fa4531
DogeDios <109776784+thanyapatJul@users.noreply.github.com>
2024-12-25 14:40:27 +0700

    [Ref] - Frist edit

--- scg_wms/scg_wms/settings.py
+++ scg_wms/scg_wms/settings.py
@@ -7,7 +7,7 @@ from logging.handlers import TimedRotatingFileHandler
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+#asdasdasdasdasdas
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
